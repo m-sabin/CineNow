c218e7896b3bb4298fd5d03e99eab1e72b074ed3
nibass <contato.ecomcenter@gmail.com>
2025-05-07 14:55:03 -0300

    Create ApiService

---
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -0,0 +1,9 @@
+package com.devspacecinenow
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("top_rated?language=en-US&page=1")
+    fun getTopRatedMovies() : Call<List<Unit>>
+}
