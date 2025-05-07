9bb7bc47a881f96940b1ac1409a7959caece9492
nibass <contato.ecomcenter@gmail.com>
2025-05-07 15:12:43 -0300

    Create httpClient to intercept the data ande place  the token

--- app/src/main/java/com/devspacecinenow/RetrofitClient.kt
+++ app/src/main/java/com/devspacecinenow/RetrofitClient.kt
@@ -1,12 +1,30 @@
 package com.devspacecinenow
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
+    private val httpClient: OkHttpClient
+        get() {
+            val clientBuilder = OkHttpClient.Builder()
+            val token = ""
+
+            clientBuilder.addInterceptor { chain ->
+                val original: Request = chain.request()
+                val requestBuilder: Request.Builder = original.newBuilder()
+                    .header("Authorization", "Bearer $token")
+                val request: Request = requestBuilder.build()
+                chain.proceed(request)
+            }
+            return clientBuilder.build()
+        }
+
     val retrofitInstance: Retrofit = Retrofit.Builder()
         .baseUrl("")
+        .client(httpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
