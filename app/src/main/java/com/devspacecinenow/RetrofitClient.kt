c3a7d14e7d07c95377548afe82fb7498aa2134b2
nibass <contato.ecomcenter@gmail.com>
2025-05-09 14:40:41 -0300

    Api setup completed

--- app/src/main/java/com/devspacecinenow/RetrofitClient.kt
+++ app/src/main/java/com/devspacecinenow/RetrofitClient.kt
@@ -10,7 +10,7 @@ object RetrofitClient {
     private val httpClient: OkHttpClient
         get() {
             val clientBuilder = OkHttpClient.Builder()
-            val token = ""
+            val token = BuildConfig.API_KEY
 
             clientBuilder.addInterceptor { chain ->
                 val original: Request = chain.request()
