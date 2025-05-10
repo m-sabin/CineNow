00703b412029a691347683056af7442c2d8a6808
nibass <contato.ecomcenter@gmail.com>
2025-05-10 13:15:46 -0300

    Consuming the API bringing the Now Playing images

--- app/src/main/java/com/devspacecinenow/ApiService.kt
+++ app/src/main/java/com/devspacecinenow/ApiService.kt
@@ -4,6 +4,10 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiService {
+
     @GET("top_rated?language=en-US&page=1")
     fun getTopRatedMovies() : Call<MovieResponse>
+
+    @GET("now_playing?language=en-US&page=1")
+    fun getNowPlayingMovies() : Call<MovieResponse>
 }
--- app/src/main/java/com/devspacecinenow/MainActivity.kt
+++ app/src/main/java/com/devspacecinenow/MainActivity.kt
@@ -5,14 +5,19 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,7 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -38,9 +42,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CineNowTheme {
-                var topRatedMovies by remember { mutableStateOf<List<MovieDto>>(emptyList()) }
-
                 val apiService = RetrofitClient.retrofitInstance.create(ApiService::class.java)
+
+                var topRatedMovies by remember { mutableStateOf<List<MovieDto>>(emptyList()) }
+                var nowPlayingMovies by remember { mutableStateOf<List<MovieDto>>(emptyList()) }
+
+
                 val callTopRated = apiService.getTopRatedMovies()
 
                 callTopRated.enqueue(object : Callback<MovieResponse> {
@@ -54,7 +61,7 @@ class MainActivity : ComponentActivity() {
                                 topRatedMovies = movies
                             }
                         } else {
-                            Log.d("Main Activity", "Erro :: ${response.errorBody()}")
+                            Log.d("Main Activity", "Error :: ${response.errorBody()}")
                         }
                     }
 
@@ -64,7 +71,30 @@ class MainActivity : ComponentActivity() {
                     ) {
                         Log.d("Main Activity", "Network Error ${t.message}")
                     }
+                })
 
+                val callNowPlaying = apiService.getNowPlayingMovies()
+                callNowPlaying.enqueue(object : Callback<MovieResponse>{
+                    override fun onResponse(
+                        call: Call<MovieResponse?>,
+                        response: Response<MovieResponse?>
+                    ) {
+                        if (response.isSuccessful){
+                            val movies = response.body()?.results
+                            if (movies != null){
+                                nowPlayingMovies = movies
+                            }
+                        } else {
+                            Log.d("Main Activity", "error :: ${response.errorBody()}")
+                        }
+                    }
+
+                    override fun onFailure(
+                        call: Call<MovieResponse?>,
+                        t: Throwable
+                    ) {
+                        Log.d("Main Activity", "Network error ${t.message}")
+                    }
                 })
 
                 // A surface container using the 'background' color from the theme
@@ -72,14 +102,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MovieList(
-                        topRatedMovies
-                    ) {movieClicked ->
-
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        Text(
+                            modifier = Modifier.padding(8.dp),
+                            text = "CineNow",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 40.sp
+                        )
+                        MovieSession(
+                            label = "Top Rated",
+                            movieList = topRatedMovies,
+                            onClick = { movieClicked -> }
+                        )
+                        MovieSession(
+                            label = "Now Playing",
+                            movieList = nowPlayingMovies,
+                            onClick = {movieClicked -> }
+                        )
                     }
+
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MovieSession(
+    label: String,
+    movieList: List<MovieDto>,
+    onClick: (MovieDto) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(
+            fontSize = 24.sp,
+            text = label,
+            fontWeight = FontWeight.SemiBold
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        MovieList(movieList = movieList, onClick = onClick)
     }
 }
 
@@ -106,7 +175,7 @@ fun MovieItem(
     onClick: (MovieDto) -> Unit
 ) {
     Column(
-        modifier = Modifier.clickable{
+        modifier = Modifier.clickable {
             onClick.invoke(movieDto)
         }
     ) {
